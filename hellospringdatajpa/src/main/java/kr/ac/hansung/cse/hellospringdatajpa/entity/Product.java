05c98df5e1d4208c05b5001975106a56c49cdcf9
reader-wh94 <sstt9884@gmail.com>
2021-05-12 12:31:28 +0900

    Study Spring Data JPA

---
+++ hellospringdatajpa/src/main/java/kr/ac/hansung/cse/hellospringdatajpa/entity/Product.java
@@ -0,0 +1,32 @@
+package kr.ac.hansung.cse.hellospringdatajpa.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "app_product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String brand;
+    private String madein;
+    private double price;
+
+    public Product(String name, String brand, String madein, double price) {
+        this.name = name;
+        this.brand = brand;
+        this.madein = madein;
+        this.price = price;
+    }
+}
---
+++ hellospringdatajpa/src/main/java/kr/ac/hansung/cse/hellospringdatajpa/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package kr.ac.hansung.cse.hellospringdatajpa.repository;
+
+import kr.ac.hansung.cse.hellospringdatajpa.entity.Product;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+    Product findByName(String name);
+
+    List<Product> findByNameContaining(String searchKeyword, Pageable paging);
+
+    @Query("select p from Product p where p.name like %?1%")
+    List<Product> searchByName(String name);
+}
