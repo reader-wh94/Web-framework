fe90572b0864df1da02716029287be3260af80c9
reader-wh94 <sstt9884@gmail.com>
2021-06-02 22:21:40 +0900

    Study Spring Hateoas

---
+++ helloSpringHateoas/src/main/java/kr/ac/hansung/cse/hellospringhateoas/repository/AlbumRepository.java
@@ -0,0 +1,8 @@
+package kr.ac.hansung.cse.hellospringhateoas.repository;
+
+import kr.ac.hansung.cse.hellospringhateoas.entity.AlbumEntity;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface AlbumRepository extends PagingAndSortingRepository<AlbumEntity, Long> {
+
+}
